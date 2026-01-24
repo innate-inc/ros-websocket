df95ecea37e94d1dd36bd3ea650d198e83a1b0fb
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2026-01-23 17:11:47 -0800

    no utf8 on outgoing supposedly

--- src/server_node.cpp
+++ src/server_node.cpp
@@ -22,31 +22,16 @@
 #include "rws/connector.hpp"
 #include "rws/node_interface_impl.hpp"
 
-// Custom websocketpp config that disables UTF-8 validation for outgoing messages
-// Since we're generating valid JSON, we know the output is valid UTF-8
-struct asio_no_utf8_check : public websocketpp::config::asio {
-  // Disable UTF-8 validation (we handle encoding ourselves)
-  static const bool enable_multithreading = true;
-  
-  struct permessage_deflate_config {};
-  
-  typedef websocketpp::extensions::permessage_deflate::disabled<permessage_deflate_config>
-    permessage_deflate_type;
-  
-  // Override the UTF-8 validation setting
-  static const websocketpp::log::level alog_level = websocketpp::log::alevel::access_core;
-  static const websocketpp::log::level elog_level = websocketpp::log::elevel::none;
-  
-  // Disable outgoing UTF-8 validation
-  static const bool validate_utf8_outgoing = false;
-};
-
 using websocketpp::connection_hdl;
 using websocketpp::lib::bind;
 using websocketpp::lib::condition_variable;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
-typedef websocketpp::server<asio_no_utf8_check> server;
+
+// Note: WebSocket++ hybi13.hpp is patched via CMakeLists.txt to skip outgoing UTF-8 validation.
+// This improves performance since we generate valid UTF-8 JSON.
+// The binary_frames_only parameter controls whether to accept only binary frames from clients.
+typedef websocketpp::server<websocketpp::config::asio> server;
 
 enum action_type {
   SUBSCRIBE,
